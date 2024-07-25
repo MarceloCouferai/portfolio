cd0cc208892dab352768e3dc280bb0a444a1e5bf
Marcelo <mcouferai@gmail.com>
2024-07-25 15:23:58 -0300

    Finalizando estilização nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
-import "./styles/App.css"
 import "./styles/reset.css"
+import "./styles/App.css"
+import React from "react";
+//import { Loading } from "./components/Loading/Loading";
 import { Navbar } from "./components/Navbar/Navbar"
+import { Loading } from "./components/Loading/Loading";
 
 
 export class App extends React.Component {
     render(){
         return(
             <> 
-                <Navbar />
+                {/* <Navbar /> */}
+                <Loading />
             </>
-            
         )
     }
 }
---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import "./Loading.css"
+
+export function Loading(){
+    return(
+        <main>
+            <div id="container">
+                <h2>ELEVANDO SEUS PROJETOS AO</h2>
+                <h1>PRÓXIMO NÍVEL</h1>
+            </div>
+        </main>
+    )
+}
