8a0c8a3724186820877afe1eba744146df57631e
Marcelo <mcouferai@gmail.com>
2024-07-29 16:01:48 -0300

    Finalizando icons

--- src/components/IconsAcademic/IconsAcademic.jsx
+++ src/components/IconsAcademic/IconsAcademic.jsx
@@ -17,7 +17,7 @@ import word from '../../assets/palavra.png';
 export function IconsAcademic(){
     AOS.init();
     return(
-        <section  data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
+        <section  data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000" className='section-IconsAcademic'>
             <div id="title">
                 <p>Além dessas formações concluídas e em andamento, foram feitos outras formações extra-curriculares e experiência profissional, adquirindo conhecimento nos seguintes itens:</p>
             </div>
