3f16caaaf0faa6110a75d661999693d55c1d7928
Marcelo <mcouferai@gmail.com>
2024-07-31 17:13:23 -0300

    Key validada

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,6 @@ import knn from './assets/knn.png'
 import etec from './assets/etec.png'
 
 export function App() {
-
-
-    console.log(process.env.REACT_APP_API_URL_SHEETMONKEY);
     const [loader, setLoader] = useState(true)
 
     useEffect(() => {
