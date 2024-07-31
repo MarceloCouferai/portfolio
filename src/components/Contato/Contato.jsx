93fe90f37e3654c7bbf9354f804584d7f98ee73e
Marcelo <mcouferai@gmail.com>
2024-07-31 15:44:18 -0300

    teste alert

--- src/components/Contato/Contato.jsx
+++ src/components/Contato/Contato.jsx
@@ -3,9 +3,29 @@ import AOS from 'aos';
 
 export function Contato(){
     AOS.init();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        const formData = new FormData(event.target);
+    
+        // Envie os dados do formulário para o Sheet Monkey
+        const response = await fetch(process.env.REACT_APP_API_URL_SHEETMONKEY, {
+          method: 'POST',
+          body: formData,
+        });
+    
+        if (response.ok) {
+          // Exibe um alerta de sucesso
+          alert('Formulário enviado com sucesso!');
+        } else {
+          // Trate erros aqui se necessário
+          alert('Erro ao enviar o formulário. Tente novamente mais tarde.');
+        }
+      };
     return(
-            <form id="formAncora" action="https://api.sheetmonkey.io/form/6LL6cNVwnJDKb1sgdfwgQj" data-aos="fade-up"
-            data-aos-anchor-placement="bottom-bottom" method="post">
+            <form id="formAncora" data-aos="fade-up"
+            data-aos-anchor-placement="bottom-bottom" onSubmit={handleSubmit}>
                     <h3>Entre em contato!</h3>
                 <div className='container-form'>
                     <div className='container-form-option'>
@@ -20,6 +40,11 @@ export function Contato(){
                         <label htmlFor="">Mensagem</label>
                         <textarea name="mensagem" id=""></textarea>
                     </div>
+                    <input 
+                        type="hidden" 
+                        name="x-sheetmonkey-redirect" 
+                        value="https://my-site.com/signup-success.html" 
+            />
                     <button type="submit">Enviar</button>
                 </div>
             </form>
